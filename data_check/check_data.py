ec5c6bdd367458f49aaf2c5db04be34555c11bca
YHaJung <hajung3768@gmail.com>
2023-06-20 15:40:13 +0900

    Add func : pick new capt idx

--- data_check/check_data.py
+++ data_check/check_data.py
@@ -14,21 +14,27 @@ from utils.compare import find_diff_flags, word_count, find_different_words
 from utils.user_io import ask_key_to_user
 
 origin_filename, new_filename = 'data_check/origin_caps/original_caps_fixed.txt', 'data_check/new_caps/final_same_caps_ver2.txt'
-diff_pairs_path = 'data_check/different_pairs.json'
-sim_pairs_path = 'data_check/similar_pairs.json'
 start_idx_path = 'data_check/start_idx.txt'
-local_dict_path = 'utils/translator.json'
-strange_idxes_path = 'data_check/strange_idxes.txt'
+# diff_pairs_path = 'data_check/different_pairs.json'
+# sim_pairs_path = 'data_check/similar_pairs.json'
+# local_dict_path = 'utils/translator.json'
+# strange_idxes_path = 'data_check/strange_idxes.txt'
 
+def save_results(checked_lines, next_idx):
+    checked_data = "\n".join(checked_lines)
+    save_file(checked_data, new_filename)
 
 def check_lines(origin_data, new_data, start_idx=0):
-    next_idx = start_idx
+
     if len(origin_data) != len(new_data):
         print('[wrong inputs] different length')
-        return origin_data, new_data, next_idx
+        return new_data, start_idx
     
+    checked_data = new_data
     for line_idx, (origin_capt, new_line) in enumerate(zip(origin_data, new_data)):
-        
+        if line_idx < start_idx:
+            continue
+
         if new_line[:5] == 'none,':
             result_key = 'none'
             new_capts = [new_line.lstrip('none, ')]
@@ -37,10 +43,25 @@ def check_lines(origin_data, new_data, start_idx=0):
             new_capts = new_line.split('+')[:-1]
 
         print(f'\n[line {line_idx}]')
+
+        # find and print different word
         diff_word_idxes, origin_capt_print, new_capts_print = find_different_words(origin_capt, new_capts)        
         print(f'[origin] {origin_capt_print}')
         for capt_idx, new_capt in enumerate(new_capts_print):
-            print(f'[{result_key} {capt_idx}] {new_capt}')
+            print(f'[{result_key} {capt_idx+1}] {new_capt}')
+
+        # pick sentence
+        work_key = '-1'
+        while (not work_key.isdigit() or int(work_key) not in range(len(new_capts)+1)) and work_key not in ['s']:
+            work_key = input('[save(s)]\nPick caption idx ')
+        if work_key == 's':
+            return checked_data, line_idx
+        checked_data[line_idx] = new_capts[int(work_key)-1]
+
+    return checked_data, line_idx
+        
+
+        
 
 
 if __name__=='__main__':
@@ -48,6 +69,6 @@ if __name__=='__main__':
     new_data = load_file(new_filename)
 
     start_idx = int(load_file(start_idx_path)[0])
-    print(f'start with line {start_idx}')
 
-    check_lines(origin_data, new_data, start_idx)
+    checked_data, next_idx = check_lines(origin_data, new_data, start_idx)
+    save_results(checked_data, next_idx)
