4c9f4c05d902edb0707488b45636de6d2f609cc4
YHaJung <hajung3768@gmail.com>
2023-06-20 14:23:13 +0900

    Add func : print inputs

---
+++ data_check/check_data.py
@@ -0,0 +1,52 @@
+import pandas as pd
+import cv2
+import random
+
+import sys, os
+root_path = os.getcwd()
+print(f'root path : {root_path}')
+sys.path.append(root_path)
+
+from utils.file_processing import increment_filename, save_file, load_file
+from data_check.sub_infos.category import cate
+from utils.translate import translate_to_korean_local, translate_to_korean
+from utils.compare import find_diff_flags, word_count
+from utils.user_io import ask_key_to_user
+
+origin_filename, new_filename = 'data_check/origin_caps/original_caps_fixed.txt', 'data_check/new_caps/final_same_caps_ver2.txt'
+diff_pairs_path = 'data_check/different_pairs.json'
+sim_pairs_path = 'data_check/similar_pairs.json'
+start_idx_path = 'data_check/start_idx.txt'
+local_dict_path = 'utils/translator.json'
+strange_idxes_path = 'data_check/strange_idxes.txt'
+
+
+def check_lines(origin_data, new_data, start_idx=0):
+    next_idx = start_idx
+    if len(origin_data) != len(new_data):
+        print('[wrong inputs] different length')
+        return origin_data, new_data, next_idx
+    
+    for line_idx, (origin_capt, new_line) in enumerate(zip(origin_data, new_data)):
+        if new_line[:5] == 'none,':
+            result_key = 'none'
+            new_capts = [new_line.lstrip('none, ')]
+        else:
+            result_key = '+'
+            new_capts = new_line.split('+')[:-1]
+
+        
+        print(f'\nline {line_idx}')
+        print(origin_capt)
+        for new_capt in new_capts:
+            print(f'[{result_key}] {new_capt}')
+
+
+if __name__=='__main__':
+    origin_data = load_file(origin_filename)
+    new_data = load_file(new_filename)
+
+    start_idx = int(load_file(start_idx_path)[0])
+    print(f'start with line {start_idx}')
+
+    check_lines(origin_data, new_data, start_idx)
--- utils/compare.py
+++ utils/compare.py
@@ -6,3 +6,8 @@ def find_diff_flags(origin_words, new_words):
         if origin_words[word_idx] != new_words[word_idx]:
             diff_flag_list[word_idx] = 1
     return diff_flag_list
+
+
+def word_count(line):
+    words = line.split(' ')
+    return len(words)
